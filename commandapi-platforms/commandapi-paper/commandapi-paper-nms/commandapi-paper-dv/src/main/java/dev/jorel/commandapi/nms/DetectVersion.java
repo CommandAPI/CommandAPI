40910d106dd86404102c7db7956967ea75b1ba59
DerEchtePilz <81232921+DerEchtePilz@users.noreply.github.com>
2025-05-20 12:51:58 +0200

    Make DetectVersion future-proof by using API when applicable

--- commandapi-platforms/commandapi-paper/commandapi-paper-nms/commandapi-paper-dv/src/main/java/dev/jorel/commandapi/nms/DetectVersion.java
+++ commandapi-platforms/commandapi-paper/commandapi-paper-nms/commandapi-paper-dv/src/main/java/dev/jorel/commandapi/nms/DetectVersion.java
@@ -1,5 +1,6 @@
 package dev.jorel.commandapi.nms;
 
+import io.papermc.paper.ServerBuildInfo;
 import net.minecraft.SharedConstants;
 
 public class DetectVersion {
@@ -7,7 +8,13 @@ public class DetectVersion {
 	private DetectVersion() {}
 
 	public static String getVersion() {
-		return SharedConstants.getCurrentVersion().getId();
+		try {
+			return ServerBuildInfo.buildInfo().minecraftVersionId();
+		} catch (Throwable t) {
+			// We don't even care what the exception was, we just know that if it throws, we are on a version that doesn't have
+			// the ServerBuildInfo so we can fall back to internals which are all the same on versions on and before 1.20.6
+			return SharedConstants.getCurrentVersion().getId();
+		}
 	}
 
 }
