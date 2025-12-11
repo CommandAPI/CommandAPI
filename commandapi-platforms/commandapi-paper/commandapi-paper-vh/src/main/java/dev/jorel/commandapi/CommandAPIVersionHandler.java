6c6156295cc0b36e1e4401cf7f7c116527dd4284
Thomas Marchand <thomas.marchand@tuta.io>
2025-12-11 22:08:34 +0000

    feat: add support for 1.21.11

--- commandapi-platforms/commandapi-paper/commandapi-paper-vh/src/main/java/dev/jorel/commandapi/CommandAPIVersionHandler.java
+++ commandapi-platforms/commandapi-paper/commandapi-paper-vh/src/main/java/dev/jorel/commandapi/CommandAPIVersionHandler.java
@@ -31,33 +31,36 @@ public abstract class CommandAPIVersionHandler {
 				case "1.21.4" -> new PaperNMS_1_21_R3();
 				case "1.21.5" -> new PaperNMS_1_21_R4();
 				case "1.21.6", "1.21.7", "1.21.8" -> new PaperNMS_1_21_R5();
-				case "1.21.9", "1.21.10" -> new PaperNMS_1_21_R6();
+				case "1.21.9", "1.21.10", "1.21.11" -> new PaperNMS_1_21_R6();
 				default -> null;
 			};
 			if (versionAdapter != null) {
 				return new LoadContext(
-					new CommandAPIPaper<>(internalPaperConfig, new APITypeProvider(versionAdapter)),
-					() -> CommandAPI.logNormal("Loaded version " + CommandAPI.getPlatformMessage(versionAdapter))
-				);
+						new CommandAPIPaper<>(internalPaperConfig, new APITypeProvider(versionAdapter)),
+						() -> CommandAPI.logNormal("Loaded version " + CommandAPI.getPlatformMessage(versionAdapter)));
 			}
 			if (CommandAPIPaper.getConfiguration().fallbackToLatestNMS()) {
 				PaperNMS<?> paperNMS = new PaperNMS_1_21_R6();
 				return new LoadContext(
-					new CommandAPIPaper<>(internalPaperConfig, new APITypeProvider(paperNMS)),
-					() -> {
-						CommandAPI.logNormal("Loaded version " + CommandAPI.getPlatformMessage(paperNMS));
-						CommandAPI.logWarning("Loading the CommandAPI with the latest and potentially incompatible NMS implementation.");
-						CommandAPI.logWarning("While you may find success with this, further updates might be necessary to fully support the version you are using.");
-					}
-				);
+						new CommandAPIPaper<>(internalPaperConfig, new APITypeProvider(paperNMS)),
+						() -> {
+							CommandAPI.logNormal("Loaded version " + CommandAPI.getPlatformMessage(paperNMS));
+							CommandAPI.logWarning(
+									"Loading the CommandAPI with the latest and potentially incompatible NMS implementation.");
+							CommandAPI.logWarning(
+									"While you may find success with this, further updates might be necessary to fully support the version you are using.");
+						});
 			}
 			version = buildInfo.asString(ServerBuildInfo.StringRepresentation.VERSION_SIMPLE);
 			throw new UnsupportedVersionException(version);
 		} catch (Throwable error) {
 			if (error instanceof ClassNotFoundException cnfe) {
-				// Thrown when users use versions before the ServerBuildInfo was added. We should inform them
+				// Thrown when users use versions before the ServerBuildInfo was added. We
+				// should inform them
 				// to update since Paper 1.20.6 was experimental then anyway
-				throw new IllegalStateException("The CommandAPI doesn't support any version before Paper 1.20.6 build 79. Please update your server!", cnfe);
+				throw new IllegalStateException(
+						"The CommandAPI doesn't support any version before Paper 1.20.6 build 79. Please update your server!",
+						cnfe);
 			} else {
 				throw error;
 			}
