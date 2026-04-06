4f65c9d7ca2b1ba71ccd9134ba39f3bc1c66e8b5
DerEchtePilz <81232921+DerEchtePilz@users.noreply.github.com>
2026-04-06 23:40:30 +0200

    Create spigot mapped modules

--- commandapi-platforms/commandapi-paper/commandapi-paper-shade/build.gradle.kts
+++ commandapi-platforms/commandapi-paper/commandapi-paper-shade/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
 	id("buildlogic.java-conventions")
 }
@@ -9,4 +11,15 @@ dependencies {
 	api(project(":commandapi-paper-vh"))
 	api(project(":commandapi-paper-nms-dependency"))
 	api(project(":commandapi-paper-mojang-mapped"))
+}
+
+tasks.withType<ShadowJar> {
+	relocate("org.bukkit.craftbukkit.v1_20_R4", "org.bukkit.craftbukkit")
+	relocate("org.bukkit.craftbukkit.v1_21_R1", "org.bukkit.craftbukkit")
+	relocate("org.bukkit.craftbukkit.v1_21_R2", "org.bukkit.craftbukkit")
+	relocate("org.bukkit.craftbukkit.v1_21_R3", "org.bukkit.craftbukkit")
+	relocate("org.bukkit.craftbukkit.v1_21_R4", "org.bukkit.craftbukkit")
+	relocate("org.bukkit.craftbukkit.v1_21_R5", "org.bukkit.craftbukkit")
+	relocate("org.bukkit.craftbukkit.v1_21_R6", "org.bukkit.craftbukkit")
+	relocate("org.bukkit.craftbukkit.v1_21_R7", "org.bukkit.craftbukkit")
 }
