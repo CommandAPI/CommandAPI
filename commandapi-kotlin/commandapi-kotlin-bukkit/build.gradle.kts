45c5c6fd2a73fb44d8e8d76c374cbbc777f414c6
DerEchtePilz <81232921+DerEchtePilz@users.noreply.github.com>
2026-04-07 17:07:45 +0200

    Fix Kotlin DSL build

--- commandapi-kotlin/commandapi-kotlin-bukkit/build.gradle.kts
+++ commandapi-kotlin/commandapi-kotlin-bukkit/build.gradle.kts
@@ -1,5 +1,8 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+
 plugins {
 	id("buildlogic.java-conventions")
+	kotlin("jvm")
 }
 
 description = "Bukkit support in Kotlin"
@@ -8,6 +11,10 @@ dependencies {
 	compileOnly(libs.paper.version.v1201)
 	compileOnly(libs.org.jetbrains.kotlin.kotlin.stdlib)
 
-	compileOnly(project(":commandapi-kotlin-core"))
+	implementation(project(":commandapi-kotlin-core"))
 	compileOnly(project(":commandapi-bukkit-core"))
+}
+
+kotlin {
+	jvmToolchain(17)
 }
