533aa0a2ec85661cfc4cdae6dbadf8adf87f8d10
DerEchtePilz <81232921+DerEchtePilz@users.noreply.github.com>
2026-06-21 22:41:58 +0200

    Fix shading problems

--- commandapi-platforms/commandapi-velocity/commandapi-velocity-plugin/build.gradle.kts
+++ commandapi-platforms/commandapi-velocity/commandapi-velocity-plugin/build.gradle.kts
@@ -1,3 +1,6 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.gradle.kotlin.dsl.withType
+
 plugins {
 	id("buildlogic.java-conventions")
 }
@@ -6,7 +9,11 @@ description = "Velocity support plugin"
 
 dependencies {
 	compileOnly(paper.velocity.api)
+	annotationProcessor(paper.velocity.api)
 
 	implementation(project(":commandapi-velocity-core"))
 	implementation(project(":commandapi-plugin"))
+}
+
+tasks.withType<ProcessResources> {
 }
